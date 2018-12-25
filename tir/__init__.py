004ab82f45b853a7b50e919409bab59afdeb61c7
Pouriya <pouriya.jahanbakhsh@gmail.com>
2018-12-25 14:06:14 +0000

    Revert "fix setup.py file"

--- tir/__init__.py
+++ tir/__init__.py
@@ -1,3 +1,8 @@
 from __future__ import absolute_import
 
+__version__     = "18.11.9"
+__description__ = "have time.ir in terminal"
+__author__      = "Pouriya Jahanbakhsh"
+__email__       = "pouriya.jahanbakhsh@gmail.com"
+
 from .tir import *
